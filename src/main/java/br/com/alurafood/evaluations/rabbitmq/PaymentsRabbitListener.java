45b01b7619677c590744daced78d64ebd3ff0e22
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-08-11 13:35:08 +0100

    config bindings in exchanges by rabbitmq

--- src/main/java/br/com/alurafood/evaluations/rabbitmq/PaymentsRabbitListener.java
+++ src/main/java/br/com/alurafood/evaluations/rabbitmq/PaymentsRabbitListener.java
@@ -2,13 +2,15 @@ package br.com.alurafood.evaluations.rabbitmq;
 
 import br.com.alurafood.evaluations.dto.PaymentDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentsRabbitListener {
 
     @RabbitListener(queues = "alura-food.payments-ms.payments-details.evaluations-ms")
-    public void getPaymentsMessages(PaymentDto payment){
+    public void getPaymentsMessages(@Payload PaymentDto payment){
         System.out.println(payment.toString());
     }
+
 }
